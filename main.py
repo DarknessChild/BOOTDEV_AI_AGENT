31957303066ed9383cdd33629ca79139b5cd1f28
DarknessChild <m3g4_1337_h3r0@hotmail.com>
2026-02-11 10:21:55 -0500

    Added Token Counter

--- main.py
+++ main.py
@@ -13,7 +13,12 @@ def main():
     response = client.models.generate_content(
     model='gemini-2.5-flash', contents='"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."?'
     )
-    print(response.text)
+    if response.usage_metadata:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        raise RuntimeError ("Usage metadata not available for this response.")
+    print(f"Response: {response.text}")
 
 if __name__ == "__main__":
     main()
