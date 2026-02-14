b7fe841f08dfd8d5eded92976502eb06ca6b0aab
DarknessChild <m3g4_1337_h3r0@hotmail.com>
2026-02-14 15:55:52 -0500

    added verbose flag to argparse

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 try:
@@ -12,18 +13,25 @@ except RuntimeError:{"Key GEMINI_API_KEY does not exist  in .env."}
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=args.user_prompt
+    model='gemini-2.5-flash', contents=messages
     )
-    if response.usage_metadata:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose == True:
+        if response.usage_metadata:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+            print(f"Response: {response.text}")
+        else:
+            raise RuntimeError ("Usage metadata not available for this response.")
+
     else:
-        raise RuntimeError ("Usage metadata not available for this response.")
-    print(f"Response: {response.text}")
+        print(f"Response: {response.text}")
 
 if __name__ == "__main__":
     main()
