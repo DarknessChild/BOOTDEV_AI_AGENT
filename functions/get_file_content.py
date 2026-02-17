e2e573fae30246dd603d98cd64232755364ade74
DarknessChild <m3g4_1337_h3r0@hotmail.com>
2026-02-17 15:40:21 -0500

    added file reader utility

---
+++ functions/get_file_content.py
@@ -0,0 +1,24 @@
+import os
+def get_file_content(working_directory, file_path):
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        # Will be True or False
+        valid_target_dir = os.path.commonpath([abs_working_dir, target_dir]) == abs_working_dir
+        if valid_target_dir == True:
+            if os.path.isfile(target_dir):
+                try:
+                    max_chars = 10000
+                    with open(target_dir, 'r', encoding='utf-8') as f:
+                        content = f.read(max_chars)
+                        if f.read(1):
+                            content += f'[...File "{file_path}" truncated at {max_chars} characters]'
+                        return content
+                except Exception as e:
+                    return f'Error: {e}'
+            else:
+                return f'Error: File not found or is not a regular file: "{file_path}"'
+        else:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    except Exception as e:
+        return f'Error: {e}'
