bba65dece410959e060fdb89bac77b59fe995b1a
DarknessChild <m3g4_1337_h3r0@hotmail.com>
2026-02-26 11:04:17 -0500

    testing system prompt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 load_dotenv()
 try:
@@ -19,7 +20,11 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=messages
+    model='gemini-2.5-flash',
+    contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt,
+    temperature=0
+    ),
     )
     if args.verbose == True:
         if response.usage_metadata:
